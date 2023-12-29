dddfd6c32e9fdd95e7c67815d9bb14aca34a22fe
Sabine Holler <sabine@bitcrowd.net>
2023-12-29 15:49:13 +0100

    fix menu show bug

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { Link, useParams } from "react-router-dom";
 import "../styles/menu.css";
 import { FaBars, FaTimes } from "react-icons/fa";
@@ -7,22 +7,35 @@ import LanguageSwitcher from "./LanguageSwitcher"
 
 function Menu({ isMenuOpen, setIsMenuOpen }) {
   const { lang } = useParams();
+  const [showCloseIcon, setShowCloseIcon] = useState(false);
+
+  useEffect(() => {
+    let timer;
+    if (isMenuOpen) {
+      setShowCloseIcon(true);
+    } else {
+      timer = setTimeout(() => {
+        setShowCloseIcon(false);
+      }, 300);
+    }
+    return () => clearTimeout(timer);
+  }, [isMenuOpen]);
 
   const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
+    setIsMenuOpen(!isMenuOpen)
   };
 
   const closeMenu = () => {
-    setIsMenuOpen(false);
+    setIsMenuOpen(false)
   };
 
   return (
     <div className={`menu ${isMenuOpen ? "show" : ""}`}>
       <div>
-        {isMenuOpen ? (
+        {showCloseIcon ? (
           <div className="flex justify-between px-5 mt-10">
             <h4 className="font-bold">MENU</h4>
-            <button className="menu__close-button" onClick={toggleMenu}>
+            <button className="menu__close-button" onClick={closeMenu}>
               <FaTimes />
             </button>
           </div>
