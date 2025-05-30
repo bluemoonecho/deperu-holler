f6fc9b58868e8a5fd1ff041082c8c276e5fd3b40
Sabine Holler <sabidasa@gmail.com>
2025-05-30 18:07:30 +0200

    Fix banner

--- src/components/CookieBanner.js
+++ src/components/CookieBanner.js
@@ -1,9 +1,8 @@
 import { useState } from "react";
-import { Link, useParams } from "react-router-dom";
+import { Trans } from "@lingui/macro";
 
 const CookieBanner = () => {
   const [showBanner, setShowBanner] = useState(true);
-  const { lang } = useParams();
 
   const handleClose = () => {
     setShowBanner(false);
@@ -29,8 +28,7 @@ const CookieBanner = () => {
         }}
       >
         <span style={{ flex: 1 }}>
-          Questo sito <strong>non utilizza cookie</strong> né strumenti di tracciamento. Nessun dato viene raccolto o memorizzato.{" "}
-          <Link to={`/${lang}/privacy-policy`} style={{ color: "#00f" }}>Privacy Policy</Link>
+          <Trans>Questo sito <strong>non utilizza cookie</strong> né strumenti di tracciamento. Nessun dato viene raccolto o memorizzato.</Trans>
         </span>
         <button
           onClick={handleClose}
